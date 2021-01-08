b66075394554a70196bd40c43bacd3e2e3c5c4d0
Pemberai Sweto <pembeweb@yahoo.com>
2021-01-08 21:11:15 +0200

    feat: integrate application insights

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -119,6 +119,13 @@ def index():
             # Get current values
             vote1 = r.get(button1).decode('utf-8')
             vote2 = r.get(button2).decode('utf-8')
+            
+            logger.setLevel(logging.INFO)
+            
+            if vote == 'Cats':
+                logger.info('Vote for Cats')
+            if vote == 'Dogs':
+                logger.info('Vote for Dogs')
 
             # Return results
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
